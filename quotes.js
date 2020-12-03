4d71adafb011cc9a9a4046489376e14f389e5495
Karina Kurenkova <58998102+kurenkova-dev@users.noreply.github.com>
2020-12-04 01:10:03 +0300

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,68 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+//Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide loading
+function complete (){
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+//Show New Quote
+function newQuote(){
+  loading();
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //If the author field is blank
+  if(!quote.author){
+      authorText.textContent='Uknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length
+  if(quote.text > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  //Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+//Get Quotes from api
+async function getQuotes () {
+  loading();
+  const apiUrl='https://type.fit/api/quotes';
+  try{
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+
+  }
+}
+
+// Tweet quote
+function tweetQuote(){
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+//Adding Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//on Load
+getQuotes();
